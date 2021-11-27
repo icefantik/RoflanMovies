bd59d1256f2301ae5ce1435ed878928f2ef2bb3b
petya <mityushin_petya@mail.ru>
2021-11-27 14:08:27 +0300

    develop intent in sign in and sign up

--- app/src/main/java/com/example/roflanmovies/MainActivity.kt
+++ app/src/main/java/com/example/roflanmovies/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.roflanmovies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,4 +9,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
     }
+
+    fun onClickIntentSignIn(view: android.view.View) {
+        val intent = Intent(this@MainActivity, SignInActivity::class.java)
+        startActivity(intent)
+    }
+    fun onClickIntentSignUp(view: android.view.View) {
+        val intent = Intent(this@MainActivity, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
 }
