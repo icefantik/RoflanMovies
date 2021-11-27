452e8f3e156709095683f1ea0fc233c45cdbf575
petya <mityushin_petya@mail.ru>
2021-11-27 17:01:28 +0300

    develop activity detaild movie

---
+++ app/src/main/java/com/example/roflanmovies/DetailedMovieActivity.kt
@@ -0,0 +1,11 @@
+package com.example.roflanmovies
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailedMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed_movie)
+    }
+}
--- app/src/main/java/com/example/roflanmovies/MoviesActivity.kt
+++ app/src/main/java/com/example/roflanmovies/MoviesActivity.kt
@@ -1,11 +1,18 @@
 package com.example.roflanmovies
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies_screen)
+    }
+
+    fun onClickDetalMovie(view: View) {
+        val intent = Intent(this@MoviesActivity, DetailedMovieActivity::class.java)
+        startActivity(intent)
     }
 }
